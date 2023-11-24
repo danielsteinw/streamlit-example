2d4af8ea02dc31c337cd6607b93eca1168a7ae09
danielsteinw <86968893+danielsteinw@users.noreply.github.com>
2023-11-24 12:35:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,37 +6,44 @@ import streamlit as st
 
 import streamlit as st
 
-# Initialisierung der Abschnitte als verfügbar
-abschnitt_1 = True
-abschnitt_2 = True
-abschnitt_3 = True
-abschnitt_4 = True
+def addition(a, b):
+    return a + b
 
-def reserviere_abschnitt(abschnitt):
-    global abschnitt_1, abschnitt_2, abschnitt_3, abschnitt_4
-    if abschnitt == 1:
-        abschnitt_1 = False
-    elif abschnitt == 2:
-        abschnitt_2 = False
-    elif abschnitt == 3:
-        abschnitt_3 = False
-    elif abschnitt == 4:
-        abschnitt_4 = False
+def subtraktion(a, b):
+    return a - b
+
+def multiplikation(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Kann nicht durch Null teilen"
+    else:
+        return a / b
 
 def main():
-    st.title('Reservierung Teststrecke')
+    st.title('Einfacher Taschenrechner')
 
-    st.write('Verfügbarkeit der Abschnitte:')
-    st.write(f'Abschnitt 1: {"Verfügbar" if abschnitt_1 else "Nicht verfügbar"}')
-    st.write(f'Abschnitt 2: {"Verfügbar" if abschnitt_2 else "Nicht verfügbar"}')
-    st.write(f'Abschnitt 3: {"Verfügbar" if abschnitt_3 else "Nicht verfügbar"}')
-    st.write(f'Abschnitt 4: {"Verfügbar" if abschnitt_4 else "Nicht verfügbar"}')
+    operation = st.selectbox(
+        'Wähle eine Operation',
+        ('Addition', 'Subtraktion', 'Multiplikation', 'Division')
+    )
 
-    selected_section = st.selectbox('Wähle einen Abschnitt:', [1, 2, 3, 4])
+    num1 = st.number_input('Gib die erste Zahl ein')
+    num2 = st.number_input('Gib die zweite Zahl ein')
 
-    if st.button('Abschnitt reservieren'):
-        reserviere_abschnitt(selected_section)
-        st.success(f'Abschnitt {selected_section} wurde erfolgreich reserviert!')
+    result = 0
+
+    if operation == 'Addition':
+        result = addition(num1, num2)
+    elif operation == 'Subtraktion':
+        result = subtraktion(num1, num2)
+    elif operation == 'Multiplikation':
+        result = multiplikation(num1, num2)
+    elif operation == 'Division':
+        result = division(num1, num2)
+
+    st.write(f'Das Ergebnis der {operation} ist: {result}')
 
 if __name__ == "__main__":
     main()
