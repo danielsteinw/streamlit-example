144adbc9a27fbe9374ae3d9e47cbf4d4715e6fbc
danielsteinw <86968893+danielsteinw@users.noreply.github.com>
2023-11-24 12:42:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,41 @@
 import streamlit as st
-import wikipediaapi
 import folium
 
-def fetch_wiki_info(city):
-    wiki_wiki = wikipediaapi.Wikipedia('en')
-    page = wiki_wiki.page(city)
-    if page.exists():
-        return page.summary
-    else:
-        return "Informationen nicht verfügbar."
+# Vordefinierte Informationen zu den Städten
+city_info = {
+    'New York': "New York City ist eine der größten Städte der Welt...",
+    'London': "London ist die Hauptstadt des Vereinigten Königreichs...",
+    'Paris': "Paris ist die Hauptstadt Frankreichs und eine bedeutende...",
+    'Tokyo': "Tokio ist die Hauptstadt Japans..."
+}
 
 def main():
-    st.title('Stadtinformationen aus Wikipedia')
+    st.title('Stadtinformationen')
 
-    # Koordinaten einiger Städte für die Karte
-    cities = {
+    cities = list(city_info.keys())
+    selected_city = st.selectbox('Wähle eine Stadt:', cities)
+
+    selected_city_info = city_info[selected_city]
+
+    st.write(f'### Informationen über {selected_city}')
+    st.write(selected_city_info)
+
+    # Koordinaten der Städte für die Karte
+    city_coordinates = {
         'New York': (40.7128, -74.0060),
         'London': (51.5074, -0.1278),
         'Paris': (48.8566, 2.3522),
         'Tokyo': (35.6895, 139.6917)
     }
 
-    selected_city = st.selectbox('Wähle eine Stadt:', list(cities.keys()))
-
-    city_info = fetch_wiki_info(selected_city)
-
-    st.write(f'### Informationen über {selected_city}')
-    st.write(city_info)
+    map_center = city_coordinates[selected_city]
 
     # Karte anzeigen
-    map_center = cities[selected_city]
     folium_map = folium.Map(location=map_center, zoom_start=10)
     folium.Marker(location=map_center, popup=selected_city).add_to(folium_map)
 
     st.write('### Karte')
-    folium_static(folium_map)
+    st.write(folium_map._repr_html_(), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
