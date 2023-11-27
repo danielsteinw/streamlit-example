e0bac7d3663fdffbff038c13aae521854c15224e
danielsteinw <86968893+danielsteinw@users.noreply.github.com>
2023-11-27 08:56:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,24 @@
 import streamlit as st
-import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def main():
     st.title('Gantt-Chart Beispiel')
 
-    # Dummy-Aufgabenliste
-    df = [
-        dict(Task='Aufgabe 1', Start='2023-01-01', Finish='2023-01-15'),
-        dict(Task='Aufgabe 2', Start='2023-02-01', Finish='2023-02-20'),
-        dict(Task='Aufgabe 3', Start='2023-03-01', Finish='2023-03-10')
-    ]
+    # Dummy-Daten für Gantt-Diagramm
+    data = {'Task': ['Aufgabe 1', 'Aufgabe 2', 'Aufgabe 3'],
+            'Start': ['2023-01-01', '2023-02-01', '2023-03-01'],
+            'End': ['2023-01-15', '2023-02-20', '2023-03-10']}
+    df = pd.DataFrame(data)
 
-    fig = ff.create_gantt(df)
-    st.plotly_chart(fig)
+    df['Start'] = pd.to_datetime(df['Start'])
+    df['End'] = pd.to_datetime(df['End'])
+
+    fig, ax = plt.subplots()
+    for i, task in enumerate(df['Task']):
+        ax.barh(task, df['End'][i] - df['Start'][i], left=df['Start'][i])
+
+    st.pyplot(fig)
 
 if __name__ == '__main__':
     main()
